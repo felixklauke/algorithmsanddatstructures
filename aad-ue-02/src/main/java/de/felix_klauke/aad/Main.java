ab6f8cf658319df7b9b9cad6cb28943f3106a153
Felix Klauke <info@felix-klauke.de>
2018-05-08 08:32:46 +0200

    Add fourth.

--- aad-ue-02/src/main/java/de/felix_klauke/aad/Main.java
+++ aad-ue-02/src/main/java/de/felix_klauke/aad/Main.java
@@ -2,6 +2,8 @@ package de.felix_klauke.aad;
 
 import de.felix_klauke.aad.lib.Zeitmessung;
 
+import java.util.Arrays;
+
 /**
  * @author Felix Klauke <info@felix-klauke.de>
  */
@@ -11,22 +13,18 @@ public class Main {
     private static int[] measurements = {1, 10, 100, 200, 1000, 10000, 100000};
 
     public static void main(String[] args) {
-
-        for (int measurement : measurements) {
-
+        Arrays.stream(measurements).forEach(measurement -> {
             System.out.println("Starting measurements for " + measurement);
 
             try (AutoCloseableStopUhr ignored = new AutoCloseableStopUhr("func1")) {
                 ZEITMESSUNG.func1(measurement);
             }
-
             try (AutoCloseableStopUhr ignored = new AutoCloseableStopUhr("func2")) {
                 ZEITMESSUNG.func2(measurement);
             }
-
             try (AutoCloseableStopUhr ignored = new AutoCloseableStopUhr("func6")) {
                 ZEITMESSUNG.func6(measurement);
             }
-        }
+        });
     }
 }
--- aad-ue-02/src/main/java/de/felix_klauke/aad/lib/BesterAlgorithmus.java
+++ aad-ue-02/src/main/java/de/felix_klauke/aad/lib/BesterAlgorithmus.java
@@ -4,38 +4,38 @@ import java.util.Arrays;
 
 public class BesterAlgorithmus {
 
-    private static final double[] EXECUTION_COMPLEXITY = new double[6];
+    private static double[] EXECUTION_COMPLEXITY = new double[6];
 
     // 1000n
     public static double g1(int n) {
-        return 1000 * n;
+        return (double) 1000. * n;
     }
 
     // 100n * log2(n+1)
     public static double g2(int n) {
-        return 100 * n * (Math.log10(n + 1) / Math.log10(2));
+        return (double)100. * n * Math.log10(n + 1.) / Math.log10(2.);
     }
 
     // 10 * n * n
     public static double g3(int n) {
-        return 10 * n * n;
+        return (double) 10. * n * n;
     }
 
     // n * n * n
     public static double g4(int n) {
-        return n * n * n;
+        return (double) n * n * n;
     }
 
     // 2 hoch n
     public static double g5(int n) {
-        return Math.pow(2, n);
+        return (double) Math.pow(2., n);
     }
 
     // n!
     public static double g6(int n) {
         double i = 1;
 
-        for (int i1 = 1; i1 < n; i1++) {
+        for (int i1 = 1; i1 <= n; i1++) {
             i *= i1;
         }
 
@@ -55,15 +55,17 @@ public class BesterAlgorithmus {
 
         //System.out.println("Measurment: " + n + " results: " + Arrays.toString(EXECUTION_COMPLEXITY));
 
-        int current = 0;
+        int current = 1;
 
-        for (int i = 0; i < 5; i++) {
-            if (EXECUTION_COMPLEXITY[i] < EXECUTION_COMPLEXITY[current]) {
+        for (int i = 1; i <= 6; i++) {
+            if (EXECUTION_COMPLEXITY[i - 1] < EXECUTION_COMPLEXITY[current - 1]) {
                 current = i;
             }
         }
 
-        return current + 1;
+        EXECUTION_COMPLEXITY = new double[6];
+
+        return current;
     }
 
     // Gibt f�r jede Zahl n zwischen 1 und 2000 aus, welcher der
@@ -71,27 +73,19 @@ public class BesterAlgorithmus {
     public static void main(String[] args) {
         int range = 2000;
 
-        int[] results = new int[range - 1];
+        int currentWinner = 1;
 
-        for (int i = 1; i < range; i++) {
+        for (int i = 1; i <= range; i++) {
             int winner = gewinnerFuerN(i);
 
-            results[i - 1] = winner;
+            if (winner != currentWinner) {
+                System.out.println("Winner changed. Period: " + i + ": A" + currentWinner);
 
-            System.out.println("Best algo for " + i + " is g" + winner);
-        }
-
-        System.out.println(Arrays.toString(results));
-
-        int lastStart = 0;
-        int currentResult = 0;
-        for (int i = 0; i < results.length; i++) {
-            if (currentResult != results[i] && currentResult != 0 || i == results.length - 1) {
-                System.out.println("From " + (lastStart + 1) + " to " + ((i == results.length - 1) ? "end" : (i + " " + currentResult)) + " seems to be the best.");
-                lastStart = i;
+                currentWinner = winner;
             }
-
-            currentResult = results[i];
         }
+
+        System.out.println(g1(1999));
+        System.out.println(g4(1999));
     }
 }
